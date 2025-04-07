11b9576f6e44b81761df375e186bca7eb755ebd5
svargasro <svargasro@unal.edu.co>
2025-04-07 00:13:29 -0500

    increasingIndexWorking

--- basicNummericalMethods/2BodyProblem/2BodyProblem.py
+++ basicNummericalMethods/2BodyProblem/2BodyProblem.py
@@ -87,11 +87,7 @@ def position(t):
 def orbit():
 
     T = TimeDelta((T_o).value*u.s)
-
-
     T = t_p + T
-
-
     # Diferencia total de tiempo como TimeDelta
     delta_total = T - t_p
 
@@ -163,16 +159,145 @@ def orbit():
     plt.tight_layout()
     plt.savefig('orbita_completa.pdf')
 
+def rMaxMin():
+    fmax = 2*arctanMod(np.tan(np.pi/2)*np.sqrt((1+e)/(1-e))) #Se modifica la arcotangente para garantizar la continuidad. (Soluciona la discontinuidad dada por la tangente)
+    fmin = 2*arctanMod(np.tan(0/2)*np.sqrt((1+e)/(1-e))) #Se modifica la arcotangente para garantizar la continuidad. (Soluciona la discontinuidad dada por la tangente)
+    rmin = round((a*(1-np.power(e,2))/(1+e*np.cos(fmin))).value,3)
+    rmax = round((a*(1-np.power(e,2))/(1+e*np.cos(fmax))).value,3)
+    return rmax, rmin
+
+def findIndexDate(r,r0):
+    halfIndex= int(len(r)/2)
+    rC = np.array(r[:halfIndex]) #rCreciente
+    rD = np.array(r[halfIndex:]) #rDecreciente
+
+    # t_vals = (t - t[0]).to_value('s')  # Tiempo desde t0 en segundos
+    # plt.plot(t_vals[:halfIndex], rC, label='r vs t', linewidth=2, marker='x')
+    # plt.plot(t_vals[halfIndex:], rD, label='r vs t', linewidth=2, marker='*')
+    # plt.plot(t_vals, r, label='r vs t', color='darkorange', linewidth=2, marker='.')
+    # plt.savefig('control.pdf')
+
+    if not np.all(np.diff(rC) >= 0): #Arreglo creciente.
+        raise ValueError("El arreglo r no está en orden creciente")
+    if not np.all(np.diff(rD) <= 0): #Arreglo decreciente.
+        raise ValueError("El arreglo r no está en orden decreciente")
+
+
+    idx1 = np.searchsorted(rC, r0)  #Se busca el índice a derecha, tal que r0 quede ordenado
+    idx2 = np.searchsorted(-rD, -r0)  #Se busca el índice a derecha, tal que r0 quede ordenado
+
+    #Se asegura que se hayan encontrado los índices
+    if idx1 == 0 or idx1 == len(rC):
+        raise ValueError("r0 está fuera del rango del arreglo rC")
+
+    if idx2 == 0 or idx2 == len(rD):
+        raise ValueError("r0 está fuera del rango del arreglo rD")
+
+    # print(r[idx1-1],r0,r[idx1], idx1-1)
+    # print(r[len(rC)+idx2-1],r0,r[len(rC)+idx2], len(rC)+idx2-1)
+
+    return idx1-1, len(rC)+idx2-1
+
+def quadraticInterpolation(x1, x2, x3, f1, f2, f3, x):
+    p2 = (((x-x2)*(x-x3))/((x1-x2)*(x1-x3)))*f1 + (((x-x1)*(x-x3))/((x2-x1)*(x2-x3)))*f2 + (((x-x1)*(x-x2))/((x3-x1)*(x3-x2)))*f3
+    return p2
+
 def date(r0):
     # posición radial y retorna tiempo t0 en que el satélite se localiza allí
-    pass
+    T = TimeDelta((T_o).value*u.s)
+    T = t_p + T
+    # Diferencia total de tiempo como TimeDelta
+    delta_total = T - t_p
+    steps = np.linspace(0, 1, 100)
+    delta_array = steps * delta_total  # TimeDelta array
+
+    # Sumar a la fecha inicial para obtener el array de fechas
+    t = t_p + delta_array
+
+    r,phi,f = position(t)
+
+    rmax, rmin = rMaxMin()
+
+    # rmax= round(np.max(r).value,3)
+    # rmin= round(np.min(r).value,3)
+
+    if r0>rmax or r0<rmin:
+         raise ValueError(f"El valor de r0 debe estar entre {rmin} km y {rmax} km")
+
+    rIndexIncreasing, rIndexDecreasing = findIndexDate(r,r0)
+
+    t_vals = (t - t[0]).to_value('s')  # Tiempo desde t0 en segundos
+
+
+    # def findDateGivenIndex(t_vals,rIndex):
+
+    x1,x2,x3 = t_vals[rIndexIncreasing], t_vals[rIndexIncreasing+1], t_vals[rIndexIncreasing+2]
+    f1,f2,f3 = r[rIndexIncreasing], r[rIndexIncreasing+1], r[rIndexIncreasing+2]
+
+    x = np.linspace(x1,x2,30)
+    r_interpolated = np.array(quadraticInterpolation(x1, x2, x3, f1, f2, f3,x))
+
+
+
+
+
+    closerIndex = np.argmin(np.abs(r_interpolated - r0)) #índice para el cual más nos acercamos
+    print(r_interpolated[closerIndex])
+    print(r0)
+
+    closerT = x[closerIndex] #Tiempo para el cual está en r0, medido desde tp=0
+
+    print("closerT: ",closerT)
+    print(x1)
+
+
+    date = t_p + closerT*u.s
+    print(t_p.iso)
+    print((t_p+x1*u.s).iso)
+    print(date.iso)
+
+    # Crear el gráfico
+    plt.figure(figsize=(8, 5))
+
+    # Curva interpolada
+    plt.plot(x, r_interpolated,label='Interpolación cuadrática', color='teal')
+
+    # Puntos originales
+    plt.scatter([x1, x2, x3], [f1.value, f2.value, f3.value], color='red', label='Datos originales', zorder=5)
+
+#    plt.plot(t_vals[(rIndexIncreasing-1):(rIndexIncreasing+3)], r[(rIndexIncreasing-1):(rIndexIncreasing+3)], label='r vs t', color='darkorange', marker='.')
+    plt.plot(t_vals, r, label='r vs t', color='darkorange', marker='.')
+
+    # Línea vertical en x = closerT
+    plt.axvline(x=closerT, color='gray', linestyle='--', label='x = closerT')
+
+    # Línea horizontal en y = r0
+    plt.axhline(y=r0, color='purple', linestyle='--', label='r = r0')
+
+    # Etiquetas, leyenda y guardado
+    plt.xlabel('Tiempo [s]')
+    plt.ylabel('Distancia [km]')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f'controlInterpolation.pdf')
+
+
+    # print(t.iso)
+
+
+
+
+
+
+
+
+
 
 
 
 
 def main():
     t_test = "2025-04-01 00:00:00"  # Tiempo de prueba.
-    t_test = "2025-03-31 01:02:48.293"
     t_test = Time(t_test, format='iso', scale='utc')
     t = t_test
 
@@ -182,8 +307,8 @@ def main():
 
     # r,phi,f = position(t)
     # print(r, phi.to(u.deg))
-    orbit()
-
+#    orbit()
+    date(9000)
 
 
 
