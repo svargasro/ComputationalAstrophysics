a3a121ed168df58338b0cfc3bc94395e204df34c
svargasro <svargasro@unal.edu.co>
2025-04-06 19:57:00 -0500

    solvedBisectionError

--- basicNummericalMethods/2BodyProblem/2BodyProblem.py
+++ basicNummericalMethods/2BodyProblem/2BodyProblem.py
@@ -6,6 +6,7 @@ from astropy.constants import R_earth, GM_earth
 from astropy.time import Time, TimeDelta
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+from scipy.optimize import fsolve
 
 #Variables globales
 
@@ -16,51 +17,46 @@ e = 0.16561   #Excentricidad
 w = 15* u.deg  # Argumento del pericentro
 w = w.to(u.rad)
 t_p = "2025-03-31 00:00:00"  # Tiempo en el que pasa por el pericentro (E=0)
-
-# Constante gravitacional terrestre
-GM_earth = GM_earth.to(u.km**3 / u.s**2)
-
-#Se convierte el tiempo a segundos
-t_p = Time(t_p, format='iso', scale='utc')
+t_p = Time(t_p, format='iso', scale='utc') #Se convierte el tiempo inicial a formato Time
+GM_earth = GM_earth.to(u.km**3 / u.s**2) # Constante gravitacional terrestre con masa terrestre
+T_o = 2*np.pi*np.sqrt(np.power(a,3)/GM_earth) #Periodo
 
 
 # Ecuación de Kepler:
 def keplerEquation(E, l):
     return E - e * np.sin(E) - l
 
-def bisectionMethod(l): #a: inferiorLimit ,  b:superiorLimit
-
+def bisectionMethod(l):
     #Falta justificar por qué siempre funciona con los a y b elegidos.
-
     l = l.value
     a = l - l/2
-    b = l + l/2
+    b = l + l/4
 
     maxError = np.sqrt(1e-16)
     fa = keplerEquation(a,l)
-
+    fb = keplerEquation(b,l)
     while (b-a)>maxError:
+
         m = (a+b)/2.0
         fm = keplerEquation(m,l)
-
         if (fm*fa<0):
             b=m
         else:
             a=m
             fa=fm
+        fb = keplerEquation(b,l)
+
     return (a+b)/2
 
-def vectorizedBisectionMethod(l):  #a: inferiorLimit ,  b:superiorLimit
-
+def vectorizedBisectionMethod(l):
     #Falta justificar por qué siempre funciona con los a y b elegidos.
 
     l = l.value
     a = l - l/2
-    b = l + l/2
+    b = l + l/4
 
     maxError = np.sqrt(1e-16)
     fa = keplerEquation(a,l)
-
     while not np.all((b - a) < maxError):
         m = (a + b) / 2.0
         fm = keplerEquation(m, l)
@@ -89,9 +85,8 @@ def position(t):
     return r,phi,f
 
 def orbit():
-    # T=TimeDelta((2*np.pi*np.sqrt(np.power(a,3)/GM_earth)).value*u.s)
-    T = 2*np.pi*np.sqrt(np.power(a,3)/GM_earth)
-    T = TimeDelta((T).value*u.s)
+
+    T = TimeDelta((T_o).value*u.s)
 
 
     T = t_p + T
@@ -100,7 +95,7 @@ def orbit():
     # Diferencia total de tiempo como TimeDelta
     delta_total = T - t_p
 
-    steps = np.linspace(0, 1, 100) #Rev 99
+    steps = np.linspace(0, 1, 53)
     delta_array = steps * delta_total  # TimeDelta array
 
     # Sumar a la fecha inicial para obtener el array de fechas
@@ -120,13 +115,12 @@ def orbit():
     x = r * np.cos(phi)
     y = r * np.sin(phi)
 
-
     t_vals = (t - t[0]).to_value('s')  # Tiempo desde t0 en segundos
 
 
     fig, axs = plt.subplots(3, 1, figsize=(8, 10), sharex=False)
     # 1. Órbita (x vs y)
-    axs[0].plot(x, y, label='y vs x', color='teal', linewidth=2) #, marker='o')
+    axs[0].plot(x, y, label='y vs x', color='teal', linewidth=2, marker='o')
     axs[0].set_title('Órbita del satélite a lo largo de un periodo.')
     axs[0].set_xlabel('x [km]')
     axs[0].set_ylabel('y [km]')
@@ -138,18 +132,9 @@ def orbit():
 
     # Marcar el pericentro (x[0], y[0])
 
-    axs[0].scatter(x[0], y[0], color='red', marker='*', s=100)
-    axs[0].annotate('Pericentro', (x[0].value, y[0].value), textcoords="offset points", xytext=(-40, -15), ha='left', fontsize=9)
+    axs[0].scatter(x[0].value, y[0].value, color='red', marker='*', s=100)
+    axs[0].annotate('Pericentro', (x[0].value, y[0].value), textcoords="offset points", xytext=(5, 10), ha='left', fontsize=9)
     axs[0].set_aspect('equal', adjustable='datalim')
-
-    # # Formato en notación científica para los ejes x e y
-    # axs[0].xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
-    # axs[0].xaxis.get_major_formatter().set_scientific(True)
-    # axs[0].xaxis.get_major_formatter().set_powerlimits((-1, 3))
-
-    # axs[0].yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
-    # axs[0].yaxis.get_major_formatter().set_scientific(True)
-    # axs[0].yaxis.get_major_formatter().set_powerlimits((-1, 3))
 
     axs[0].ticklabel_format(style='scientific', axis='both', scilimits=(-1,3))
 
@@ -177,24 +162,25 @@ def orbit():
     # Ajustar diseño y guardar
     plt.tight_layout()
     plt.savefig('orbita_completa.pdf')
-    # plt.show()
 
-
-
+def date(r0):
+    # posición radial y retorna tiempo t0 en que el satélite se localiza allí
+    pass
 
 
 
 
 def main():
     t_test = "2025-04-01 00:00:00"  # Tiempo de prueba.
-
+    t_test = "2025-03-31 01:02:48.293"
     t_test = Time(t_test, format='iso', scale='utc')
     t = t_test
 
 
 
 
-    # r,phi = position(t)
+
+    # r,phi,f = position(t)
     # print(r, phi.to(u.deg))
     orbit()
 
